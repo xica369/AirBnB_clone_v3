e71c67753d8e23081c060a09e67f8d1c4e1cea65
xica369 <761@holbertonschool.com>
2019-09-13 04:49:58 +0000

    update init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,5 +9,3 @@ app_views = Blueprint("mold", __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
